7587d3d5c456b7c50d7b2e8c5cf7eb8e8cb477a4
emilychavezc <44m4tzkvzg@privaterelay.appleid.com>
2026-09-04 12:07:29 -0400

    New file

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 700);
+}
+
+function draw() {
+  background(255, 0, 0);
+  fill("yellow");
+  stroke("white");
+  strokeWeight(60);
+  circle(200,200, 150);
+  
+  
+}
