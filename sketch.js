e809691de6c36044054e715604d7e6a8599d657c
emilychavezc <44m4tzkvzg@privaterelay.appleid.com>
2026-09-18 21:38:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,48 @@
+/*Name: Emily Chavez
+*/
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 900);
 }
 
 function draw() {
-  background(220);
+  background(178,255,255);
+
+ fill(255,0,0);
+ stroke(30)
+ strokeWeight(10);
+circle(200,200,300);
+
+
+ fill(0,255,0);
+ stroke(15);
+ strokeWeight(10);
+rect(400,450,300,300);
+
+ fill(0);
+ noStroke();
+rect(50,370,300,20);
+
+ fill(0);
+circle(180, 180, 8);
+circle(220, 180, 8);
+circle(180, 220, 8);
+circle(220, 220, 8);
+circle(200, 200, 8);
+
+ fill(255);
+circle(450, 600, 8);
+circle(500, 600, 8);
+circle(550, 600, 8);
+circle(600, 600, 8);
+circle(650, 600, 8);
+
+ noFill();
+ stroke(15);
+ strokeWeight(10);
+ellipse(540,610,450,450);
+
+
+ 
+
 }
