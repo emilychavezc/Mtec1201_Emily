706c18b8d31e89d0c7712b1cfacc1c19e595e900
emilychavezc <44m4tzkvzg@privaterelay.appleid.com>
2026-09-25 09:13:50 -0400

    SS2

--- SS2_EmilyC/sketch.js
+++ SS2_EmilyC/sketch.js
@@ -1,3 +1,8 @@
+let circleX = 200;
+let circleY = 200;
+let dotMove = 0;
+
+
 function setup() {
   createCanvas(800, 900);
 }
@@ -5,21 +10,31 @@ function setup() {
 function draw() {
   background(178,255,255);
 
+
+//circle follows the mouse
+circleX=mouseX;
+circleY=mouseY;
+
+
+fill(255,0,0);
+ stroke(30)
+ strokeWeight(10);
+circle(circleX,circleY,300);
+
  fill(0);
  noStroke();
 rect(50,370,300,20);
 
 
- fill(255,0,0);
- stroke(30)
- strokeWeight(10);
-circle(200,200,300);
+// One dot stays in place
+  fill(0);
+circle(circleX - 20, circleY - 20, 8);
 
- fill(0);
-circle(180, 180, 8);
-circle(220, 180, 8);
-
-
-
-
+// One dot moves inside the circle
+circle(circleX + dotMove, circleY - 20, 8);
 }
+
+function mousePressed() {
+  dotMove = dotMove + 100;
+}
+
